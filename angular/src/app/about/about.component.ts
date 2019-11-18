5f434f186aedb419b7e3de0a03556db1a39c266c
faragobruno <farago.bruno@gmail.com>
2019-11-18 14:57:42 +0100

    no message

---
+++ angular/src/app/about/about.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms'
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent implements OnInit {
+
+  messageForm: FormGroup
+  submitted =false
+  success=false
+
+  constructor(private formBuilder: FormBuilder) { 
+    this.messageForm = this.formBuilder.group({
+      name:['', Validators.required],
+      message:['', Validators.required]
+    })
+  }
+
+  onSubmit(){
+    this.submitted = true
+    if(this.messageForm.invalid){
+      return
+    }
+    this.success =true
+  }
+
+  ngOnInit() {
+  }
+
+}
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule} from '@angular/common/http'
+import { ReactiveFormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
